d0245e3c1b0a21d9e4604c62ba17921f28b0134f
M-Ainurridho <m.ainurridho11@gmail.com>
2025-01-17 21:59:39 +0700

    tambahkan method create, store, dan show

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class BookController extends Controller
@@ -14,8 +17,64 @@ class BookController extends Controller
      */
     public function index(): View
     {
+        // get all books
         $books = Book::all();
 
+        // render view with all books
         return view('books.index', compact('books'));
+    }
+
+    /**
+     * show
+     * 
+     * @param mixed $id
+     * @return View
+     */
+    public function show(string $id): View
+    {
+        // get book by id
+        $book = Book::findOrFail($id);
+
+        // render view with book by id
+        return view("books.show", compact('book'));
+    }
+
+    /**
+     * create
+     * 
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('books.create');
+    }
+
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // validation form
+        $request->validate([
+            'title'     => 'required|min:3',
+            'author'    => 'required|min:3',
+            'cover'     => 'required|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        // save image to storage
+        $image = $request->file('cover');
+        $image->storeAs('public/books', $image->hashName());
+
+        // create new book
+        Book::create([
+            "title" => $request->title,
+            "author" => $request->author,
+            "cover" => $image->hashName()
+        ]);
+
+        return redirect()->route('books.index')->with(['success' => 'Successfully Added New Book!']);
     }
 }
