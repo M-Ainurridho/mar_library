6e8fd05a23ba962af722c5c77a46c95b50405a82
M-Ainurridho <m.ainurridho11@gmail.com>
2025-01-18 15:16:03 +0700

    tambah toast

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,26 @@
     @yield('content')
   </div>
 
+  @if (session('success'))
+  <div class="toast align-items-center text-bg-success border-0 show position-fixed" style="bottom: 20px; right: 10px" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body">
+        {{ session('success') }}
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+  </div>
+  @elseif (session('error'))
+  <div class="toast align-items-center text-bg-danger border-0 show position-fixed" style="bottom: 20px; right: 10px" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body">
+        {{ session('error') }}
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+  </div>
+  @endif
+
   @include('layouts.footer')
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
