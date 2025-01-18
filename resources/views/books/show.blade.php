b80235e00c8001f8778212a5cdb8a61d39297b93
M-Ainurridho <m.ainurridho11@gmail.com>
2025-01-18 22:41:06 +0700

    pindahkan edit dan delete ke books.index

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -19,42 +19,9 @@
                 <div class="card-body">
                     <h5 class="card-title">{{ $book->title }}</h5>
                     <p class="card-text">Author : {{ $book->author }}</p>
-                    <form class="d-flex gap-2">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn-delete btn btn-danger" style="width:100%;">Delete</button>
-                        <a href="{{ route('books.edit', $book->id) }}" class="btn btn-primary" style="width: 100%;">Edit</a>
-                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
-
-@section('js')
-<script>
-    const form = document.querySelector('form')
-
-    form.addEventListener('submit', function(e) {
-        e.preventDefault()
-
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                this.setAttribute('action', "{{ route('books.destroy', $book->id) }}")
-                this.setAttribute('method', "POST")
-                this.submit()
-            }
-
-        });
-    })
-</script>
 @endsection
