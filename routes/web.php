9d1d4a855eea790e00e9428da7db03c279a78d58
M-Ainurridho <m.ainurridho11@gmail.com>
2025-01-18 21:03:45 +0700

    tambahkan bookmark buku

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, ("index")])->name('home.index');
 
 // books
-Route::resource("/books", BookController::class);
+Route::resource("books", BookController::class);
+
+// bookmarks
+Route::prefix('books')->group(function () {
+    Route::resource('bookmarks', BookmarkController::class);
+});
